0d2a7be312fd9208c68bf29b66d092c5c4d6a787
shravandoijad <shravandoijad4@gmail.com>
2025-03-31 00:08:43 +0530

    new commit

---
+++ project/my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import SignupPage from './SignupPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/LoginPage" element={<LoginPage />} />
+          <Route path="/SignupPage" element={<SignupPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
