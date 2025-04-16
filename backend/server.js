e67ea78afa2d2c8ec9a8ce94131a055ebc0278f9
shravandoijad <shravandoijad4@gmail.com>
2025-04-17 01:08:17 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -13,19 +13,9 @@ const productRouter = require("./routes/productRoute");
 const cartRouter = require('./routes/cartRoute');
 const orderRouter = require('./routes/orderRoute');
 
-const allowedOrigins = [
-    process.env.FRONTEND_URL,
-    process.env.ADMINPANEL_URL
-];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
@@ -33,7 +23,7 @@ app.use(cors({
 
 
 app.options('*', cors({
-    origin: allowedOrigins,
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
@@ -48,21 +38,21 @@ app.use("/cart", cartRouter)
 app.use("/order", orderRouter)
 const server = require("http").createServer(app)
 
-// const io = new Server(server,{
-//     cors: {
-//         origin: allowedOrigins,
-//         credentials: true
-//     }
-// })
+const io = new Server(server,{
+    cors: {
+        origin: true,
+        credentials: true
+    }
+})
 
-// io.on("connection", (socket)=>{
-//     console.log("soket", socket.id);
-//     socket.on("chat", (payload)=>{
-//         console.log("payload", payload)
-//         io.emit("chat", payload)
-//     })
+io.on("connection", (socket)=>{
+    console.log("soket", socket.id);
+    socket.on("chat", (payload)=>{
+        console.log("payload", payload)
+        io.emit("chat", payload)
+    })
     
-// })
+})
 
 
 
