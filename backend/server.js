c71f8dec6192dbf8ba566bfe63ea49ad7777557f
shravandoijad <shravandoijad4@gmail.com>
2025-04-16 22:26:20 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const cartRouter = require('./routes/cartRoute');
 const orderRouter = require('./routes/orderRoute');
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://project-manhatten-frontend.vercel.app', 'http://localhost:5174'],
     credentials:true
 }
     
@@ -31,7 +31,7 @@ const server = require("http").createServer(app)
 
 const io = new Server(server,{
     cors: {
-        origin: ['http://localhost:5173', 'http://localhost:5174'],
+        origin: ['https://project-manhatten-frontend.vercel.app', 'http://localhost:5174'],
         credentials: true
     }
 })
